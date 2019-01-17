7b75f8b591562f5e76abe26af71fa9a1e0d67f8b
Fenimore Love <exorable.ludos@gmail.com>
2019-01-16 21:39:11 -0500

    Update shell scripts
    pass argument to runtests to run a specific test,
    and use the pprof `go tool` instead of bin

--- scripts/getprofile.sh
+++ scripts/getprofile.sh
@@ -2,5 +2,5 @@ mv profiles/*_test.go .
 tmpfile=$(mktemp /tmp/cpuprof.XXXXXX)
 go test -v -cpuprofile $tmpfile
 mv *_test.go profiles/
-~/go/bin/pprof -http=":" $tmpfile
+go tool pprof -http=":" $tmpfile
 rm $tmpfile
--- scripts/runtests.sh
+++ scripts/runtests.sh
@@ -1,3 +1,15 @@
+#!/bin/bash
+
 mv tests/*_test.go .
-go test -v
+
+if [ -z "$1" ]
+then
+    go test -v
+elif [ $1 == 'bench' ]
+then
+    go test -v -bench=.
+else
+    go test -v -run $1
+fi
+
 mv *_test.go tests/
